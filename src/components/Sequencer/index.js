360427da56169370cbd7b253190d3d2b079ba885
Noelle Huchette <noelle.huchette@gmail.com>
2022-11-13 13:40:51 -0500

    moved old sequencer

--- src/components/Sequencer/index.js
+++ src/components/Sequencer/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { start, Transport, Loop, Synth, Draw, Part, Reverb } from 'tone';
 import { step, initialize, stop, play } from '../../store/sequencer';
