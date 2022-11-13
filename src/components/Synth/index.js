ccd41991a8c2842c2c20b40d98d1a592b0f11fae
Noelle Huchette <noelle.huchette@gmail.com>
2022-11-13 15:56:32 -0500

    prep for synth component, minor housekeeping

--- src/components/Synth/index.js
+++ src/components/Synth/index.js
@@ -4,7 +4,11 @@ import * as Tone from 'tone';
 import './style.css';
 
 const Synth = () => {
-  return <div></div>;
+  return (
+    <div className="synth-block">
+      <div className="synth controls"></div>
+    </div>
+  );
 };
 
 export default Synth;
